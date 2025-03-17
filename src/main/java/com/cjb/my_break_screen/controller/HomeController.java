8c64b9becd0d26decf1d8060fee00c47ca874a38
Christian Belarde <cjbelarde253@gmail.com>
2025-03-17 15:46:30 -0700

    Added button to control the color of background in real-time

--- src/main/java/com/cjb/my_break_screen/controller/HomeController.java
+++ src/main/java/com/cjb/my_break_screen/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("message", "Welcome to My Break Screen!");
-        return "index"; // Name of the HTML template
+        return "index";
     }
 }
