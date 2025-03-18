6cc403d9ad46ff3fea44e40f9a5dab60d264404d
Christian Belarde <cjbelarde253@gmail.com>
2025-03-18 15:53:08 -0700

    Added About page, added navbar to header, began building out second break screen Fire Fly

---
+++ src/main/java/com/cjb/my_break_screen/controller/ScreensController.java
@@ -0,0 +1,14 @@
+package com.cjb.my_break_screen.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ScreensController {
+    @GetMapping("/firefly")
+    public String about(Model model) {
+        return "firefly";
+    }
+}
